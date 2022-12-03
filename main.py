c86dd939ccb914b715e9b3a3dc3664fba7177361
laureno12 <laurenosays@gmail.com>
2022-12-02 22:23:00 -0500

    Created lists in main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+list_of_cities ["New York City", "Boston", "Philadelphia", "Orlando", "Cleveland", "Dallas", "Las Vegas", "Phoenix", "Los Angeles"]
+transportation_method ["car", "bus", "train", "ride share", "walk"]
+restaurant_options[]
+entertainment_options[]
