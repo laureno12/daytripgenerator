6b65c97f2db65f664fab41590e02c994a8913978
laureno12 <laurenosays@gmail.com>
2022-12-04 18:52:32 -0500

    Udpated lists and started functions

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-list_of_cities ["New York City", "Boston", "Philadelphia", "Orlando", "Cleveland", "Dallas", "Las Vegas", "Phoenix", "Los Angeles"]
-transportation_method ["car", "bus", "train", "ride share", "walk"]
-restaurant_options[]
-entertainment_options[]
+list_of_cities = ["New York City", "Boston", "Philadelphia", "Orlando", "Cleveland", "Dallas", "Las Vegas", "Phoenix", "Los Angeles"]
+transportation_method = ["car", "bus", "train", "ride share", "walk"]
+restaurant_options = [""]
+entertainment_options = ["shopping", "movie", "nature walk", "museum", "guided tour"]
+import random
+
+random_city = random.choice(list_of_cities)
+print(random_city)
+
+random_transport = random.choice(transportation_method)
+print(random_transport)
+
+random_entertainment = random.choice(entertainment_options)
+print(random_entertainment)
