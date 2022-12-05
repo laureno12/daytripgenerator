3fda17aebd744435c819ae29d80834e312338580
laureno12 <laurenosays@gmail.com>
2022-12-04 20:35:02 -0500

    Created function for cities

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ restaurant_options = [""]
 entertainment_options = ["shopping", "movie", "nature walk", "museum", "guided tour"]
 import random
 
-random_city = random.choice(list_of_cities)
-print(random_city)
+def select_city (list_of_cities):
+    confirm_city = False
+    while confirm_city == False:
+        select_city = random.choice(list_of_cities)
+        user_input = input (f"You're going to {select_city}! Do you accept? (y/n) ")
+        if user_input == "n":
+            print("No worries! Let's try again!")
+            select_city
+        else:
+            print("Awesome!")
+            confirm_city = True
+
 
 random_transport = random.choice(transportation_method)
 print(random_transport)
